7e2f2109a696cd2af159e94f3a89d92381f8bc9a
minfaox3 <spdlci30@gmail.com>
2023-07-29 21:51:45 +0900

    [add]initial content
    [add]
    index.js
    package.json
    README.md
    example.png

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # hexo-desmos-url
- Display your Desmos project as a card on hexo with mini thumbnail.
+## About
+Display your [Desmos](https://www.desmos.com/calculator) project as a card on [Hexo](http://hexo.io/) with mini thumbnail.
+
+## Installation
+`npm install --save hexo-desmos-url`
+
+## Usage
+Add the tag block to a post with the ID of the project you want to link.  
+[ID] is written in here.  
+`https://www.desmos.com/calculator/[ID]`  
+
+```
+・Minimum Style
+{% desmos_url [ID] [TITLE] %}
+
+・With description
+{% desmos_url [ID] [TITLE] [DESCRIPTION] %}
+```
+
+### Example
+`{% desmos_url af9db8ycji Fan "This graph is graphic art like a fan." %}`
+
+![example](https://github.com/minfaox3/hexo-desmos-url/example.png)
+
+## Configuration
+You can configure the card and thumbnail width, language of project page, border color and background color of card.  
+Default values are used even if no confiurations are written.  
+Width and color values can be specified in the format used in CSS.  
+The languages that can be specified depend on the languages supported by Desmos.  
+
+### Example
+```
+desmos_url:
+  lang: ja # default is en
+  card_width: 80% # default is 100%
+  thumbnail_width: 150px # default is 100px
+  background_color: "#00FF00" # default is white
+  border_color: orange # default is black
+```
+
+## LICENSE
+[MIT](https://github.com/minfaox3/hexo-desmos-url/LICENSE)
---
+++ index.js
@@ -0,0 +1,70 @@
+hexo.extend.tag.register('desmos_url', function(args) {
+    const config = hexo.config.desmos_url || {};
+    const lang = config.lang || "en";
+    const background_color = config.background_color || "white";
+    const border_color = config.border_color || "black";
+    const card_width = config.card_width || "100%";
+    const thumbnail_width = config.thumbnail_width || "100px";
+    const id = args[0];
+    const title = args[1];
+    const desc = args[2] || "&nbsp;";
+    return `
+    <div class="desmos_url">
+    <style>
+        .desmos_url {
+            position: relative;
+            transform: translateX(calc((100% - ${card_width}) / 2));
+            width: ${card_width};
+            justify-content: center;
+            display: flex;
+            border: solid 2px ${border_color};
+            border-radius: 5px;
+            border-width: 2px 4px 4px 2px;
+            background-color: ${background_color};
+        }
+        .desmos_url_thumbnail {
+            width: ${thumbnail_width};
+            min-height: ${thumbnail_width};
+            background-image:url(https://saved-work.desmos.com/calc_thumbs/production/${id}.png);
+            background-size: cover;
+            background-position: center;
+            border-top-left-radius: 2px;
+            border-top-right-radius: 2px;
+        }
+        .desmos_url_content {
+            width: calc(100% - ${thumbnail_width});
+            border-left: solid ${border_color};
+            border-top-right-radius: 2px;
+            border-bottom-right-radius: 2px;
+        }
+        .desmos_url_title {
+            height: 50px;
+            font: bold;
+            font-size: 35px;
+            width: 80%;
+        }
+        .desmos_url_border {
+            position: relative;
+            height: 2px;
+        }
+        .desmos_url_border:before{
+            content: "";
+            background-image: linear-gradient(to right, #000, #000 3px, transparent 3px, transparent 8px);
+            background-size: 8px 2px;
+            background-repeat: repeat-x;
+            position: absolute;
+            top: 0;
+            bottom: 0;
+            left: 0;
+            right: 0;
+        }
+    </style>
+        <a href="https://www.desmos.com/calculator/${id}?lang=${lang}" class="desmos_url_thumbnail"></a>
+        <div class="desmos_url_content">
+        <a href="https://www.desmos.com/calculator/${id}?lang=${lang}" class="desmos_url_thumbnail"><div class="desmos_url_title">${title}</div></a>
+            <div class="desmos_url_border"></div>
+            <div class="desmos_url_desc">${desc}</div>
+        </div>
+    </div>
+  `;
+});
